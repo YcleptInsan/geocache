69ee7dd88ce46b9413d0b5c8103f688e6cca39ce
Yclept <nickmwise@gmail.com>
2017-07-23 11:39:45 -0700

    Add forms for form pagee. No buttons yet.

--- src/app/geocache-location.service.ts
+++ src/app/geocache-location.service.ts
@@ -6,6 +6,10 @@ import { Http, Response } from '@angular/http';
 @Injectable()
 export class GeocacheLocationService {
 
-  constructor() { }
+  constructor(private http: Http) { }
+
+  getLocationsApiCall(lat: string, long: string) {
+    return this.http.get("https://maps.googleapis.com/maps/api/geocode/json?latlng=40.714224,-73.961452&key=" + geoKey)
+  }
 
 }
